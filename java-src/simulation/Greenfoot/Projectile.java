424585b4cfb46407b3c85acdd3a49ea2ba3cdc4a
Brendan Redmond <brendanrredmond@live.com>
2012-03-09 17:28:16 -0800

    Classes didn't get pushed, hopefully they did now

---
+++ java-src/simulation/Greenfoot/Projectile.java
@@ -0,0 +1,27 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * A genereric projectile
+ * 
+ * @author Brendan Redmond
+ * @version 0.0.1
+ */
+public class Projectile extends ArenaActor
+{
+    /**
+     * Constructor: calls super
+     */
+    public Projectile()
+    {
+        super();
+    }
+
+    /**
+     * Act - do whatever the Projectile wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
