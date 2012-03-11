e85ed51cc5fee8f4917aba9b0fead60fc860156c
Brendan Redmond <brendanrredmond@live.com>
2012-03-11 10:57:16 -0700

    Putting my classes up again, they were deleted somehow

---
+++ java-src/simulation/Greenfoot/ArenaActor.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Common elements of actors that act in the Arena.
+ * 
+ * @author Brendan Redmond 
+ * @version 0.0.1
+ */
+abstract public class ArenaActor extends Actor
+{
+    //the current velocity of this ArenaActor
+    private int velocity;
+
+    /**
+     * Constructor: set velocity to 0
+     */
+    public ArenaActor()
+    {
+        this.velocity = 0;
+    }
+    
+    /**
+     * Act - do whatever the ArenaActor wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
