45f400d699b19ae149c6cc704b9fb889f86caf9a
Jacob Weiss <jacob@kidzilla.com>
2012-03-29 10:21:47 -0700

    AVR now has 8 serial ports (0-7) and uses the input of connectToSerial()

--- java-src/emulator/cpu/AVR.java
+++ java-src/emulator/cpu/AVR.java
@@ -1,6 +1,7 @@
 package emulator.cpu;
 
 import emulator.wp.IO;
+import emulator.wp.ToastyIO;
 /**
  * Write a description of class AVR here.
  * 
@@ -9,7 +10,17 @@ import emulator.wp.IO;
  */
 public class AVR extends FROIDZCPU
 {    
-    public Peripheral[] peripheralMap;
+    private Peripheral[] peripheralMap;
+    private static final int[] usartLookup = {
+                                       ToastyIO.UDR0, 
+                                       ToastyIO.UDR1, 
+                                       ToastyIO.UDR2,
+                                       ToastyIO.UDR3,
+                                       ToastyIO.UDR4,
+                                       ToastyIO.UDR5,
+                                       ToastyIO.UDR6,
+                                       ToastyIO.UDR7,
+                                      };
     
     public AVR()
     {
@@ -26,7 +37,14 @@ public class AVR extends FROIDZCPU
     
     public void initializePeripherals()
     {
-        USART u = new USART(this.proc.mem, IO.UDR1, IO.UCSR1A);
+        USART u0 = new USART(this.proc.mem, IO.UDR0, IO.UCSR0A);
+        USART u1 = new USART(this.proc.mem, IO.UDR1, IO.UCSR1A);
+        USART u2 = new USART(this.proc.mem, ToastyIO.UDR2, ToastyIO.UCSR2A);
+        USART u3 = new USART(this.proc.mem, ToastyIO.UDR3, ToastyIO.UCSR3A);
+        USART u4 = new USART(this.proc.mem, ToastyIO.UDR4, ToastyIO.UCSR4A);
+        USART u5 = new USART(this.proc.mem, ToastyIO.UDR5, ToastyIO.UCSR5A);
+        USART u6 = new USART(this.proc.mem, ToastyIO.UDR6, ToastyIO.UCSR6A);
+        USART u7 = new USART(this.proc.mem, ToastyIO.UDR7, ToastyIO.UCSR7A);
         
         PWM pwm0 = new PWM(this.proc.mem, IO.OCR3AH);
         PWM pwm1 = new PWM(this.proc.mem, IO.OCR3AL);
@@ -40,7 +58,14 @@ public class AVR extends FROIDZCPU
         Port portc = new Port(this.proc.mem, 8, IO.PORTC, IO.PINC, IO.DDRC);
         Port portd = new Port(this.proc.mem, 8, IO.PORTD, IO.PIND, IO.DDRD);
         
-        this.peripheralMap[IO.UDR1] = u;
+        this.peripheralMap[IO.UDR1] = u1;
+        this.peripheralMap[IO.UDR0] = u0;
+        this.peripheralMap[ToastyIO.UDR2] = u2;
+        this.peripheralMap[ToastyIO.UDR3] = u3;
+        this.peripheralMap[ToastyIO.UDR4] = u4;
+        this.peripheralMap[ToastyIO.UDR5] = u5;
+        this.peripheralMap[ToastyIO.UDR6] = u6;
+        this.peripheralMap[ToastyIO.UDR7] = u7;
         this.peripheralMap[IO.OCR3AH] = pwm0;
         this.peripheralMap[IO.OCR3AL] = pwm1;
         this.peripheralMap[IO.OCR3BH] = pwm2;
@@ -52,7 +77,14 @@ public class AVR extends FROIDZCPU
         this.peripheralMap[IO.PORTC] = portc;
         this.peripheralMap[IO.PORTD] = portd;
         
-        this.proc.peripherals.add(u);
+        this.proc.peripherals.add(u1);
+        this.proc.peripherals.add(u0);
+        this.proc.peripherals.add(u2);
+        this.proc.peripherals.add(u3);
+        this.proc.peripherals.add(u4);
+        this.proc.peripherals.add(u5);
+        this.proc.peripherals.add(u6);
+        this.proc.peripherals.add(u7);
         this.proc.peripherals.add(pwm0);
         this.proc.peripherals.add(pwm1);
         this.proc.peripherals.add(pwm2);
@@ -67,7 +99,7 @@ public class AVR extends FROIDZCPU
 
     public void connectToSerial(IUSART usart, int udr)
     {        
-        ((USART)(this.peripheralMap[emulator.wp.IO.UDR1])).connect(usart);
+        ((USART)(this.peripheralMap[this.usartLookup[udr]])).connect(usart);
     }
     public void connectToPWM(PinConnector p, int address)
     {
